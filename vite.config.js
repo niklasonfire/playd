5d75cc3d81759b5e42e0e29dac1308cdd3135db9
Niklas <niklas@nilius.online>
2026-03-08 20:36:02 +0100

    add gh-pages deploy workflow and   flatten build output

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        assetFileNames: '[name].[ext]',
+        chunkFileNames: '[name].js',
+        entryFileNames: '[name].js',
+      },
+    },
+  }
 })
